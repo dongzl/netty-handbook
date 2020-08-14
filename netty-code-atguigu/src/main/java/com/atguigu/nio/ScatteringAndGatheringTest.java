790ef04d21384264d3db1158aa130e16998b0d14
Zonglei Dong <dzllikelsw@163.com>
2020-08-14 20:44:11 +0800

    add demo code.

---
+++ netty-code-atguigu/src/main/java/com/atguigu/nio/ScatteringAndGatheringTest.java
@@ -0,0 +1,63 @@
+package com.atguigu.nio;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
+/**
+ * Scattering：将数据写入到 buffer 时，可以采用 buffer 数组，依次写入 [分散]
+ * Gathering：从 buffer 读取数据时，可以采用 buffer 数组，依次读
+ */
+public class ScatteringAndGatheringTest {
+
+    public static void main(String[] args) throws Exception {
+        
+        //使用 ServerSocketChannel 和 SocketChannel 网络
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(7000);
+
+        //绑定端口到 socket，并启动
+        serverSocketChannel.socket().bind(inetSocketAddress);
+
+        //创建 buffer 数组
+        ByteBuffer[] byteBuffers = new ByteBuffer[2];
+        byteBuffers[0] = ByteBuffer.allocate(5);
+        byteBuffers[1] = ByteBuffer.allocate(3);
+
+        //等客户端连接 (telnet)
+        SocketChannel socketChannel = serverSocketChannel.accept();
+
+        int messageLength = 8; //假定从客户端接收 8 个字节
+
+        //循环的读取
+        while (true) {
+            int byteRead = 0;
+
+            while (byteRead < messageLength) {
+                long l = socketChannel.read(byteBuffers);
+                byteRead += 1; //累计读取的字节数
+                System.out.println("byteRead = " + byteRead);
+                //使用流打印,看看当前的这个 buffer 的 position 和 limit
+                Arrays.asList(byteBuffers).stream().map(buffer -> "position = " + buffer.position() + ", limit = " + buffer.limit()).forEach(System.out::println);
+            }
+
+            //将所有的 buffer 进行 flip
+            Arrays.asList(byteBuffers).forEach(buffer -> buffer.flip());
+            //将数据读出显示到客户端
+            long byteWirte = 0;
+            while (byteWirte < messageLength) {
+                long l = socketChannel.write(byteBuffers);//
+                byteWirte += l;
+            }
+            
+            //将所有的buffer进行clear
+            Arrays.asList(byteBuffers).forEach(buffer -> {
+                buffer.clear();
+            });
+            
+            System.out.println("byteRead = " + byteRead + ", byteWrite = " + byteWirte + ", messagelength = " + messageLength);
+        }
+    }
+}

cbb8c02ecec38505040b540d591fd1cceac92ae5
Zonglei Dong <dzllikelsw@163.com>
2020-08-17 22:23:11 +0800

    add demo code.

---
+++ netty-code-atguigu/src/main/java/com/atguigu/netty/http/TestServer.java
@@ -0,0 +1,30 @@
+package com.atguigu.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+    
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ netty-code-atguigu/src/main/java/com/atguigu/netty/simple/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.netty.simple;
+
+import io.netty.util.NettyRuntime;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}

4b925b353fbcf7196e37a21f8a04cb857627dcea
Zonglei Dong <dzllikelsw@163.com>
2020-08-20 13:39:20 +0800

    add demo code.

---
+++ netty-code-atguigu/src/main/java/com/atguigu/netty/groupchat/GroupChatClientHandler.java
@@ -0,0 +1,12 @@
+package com.atguigu.netty.groupchat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class GroupChatClientHandler extends SimpleChannelInboundHandler<String> {
+    
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(msg.trim());
+    }
+}
---
+++ netty-code-atguigu/src/main/java/com/atguigu/netty/groupchat/User.java
@@ -0,0 +1,6 @@
+package com.atguigu.netty.groupchat;
+
+public class User {
+    private int id;
+    private String pwd;
+}

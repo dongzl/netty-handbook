2ea1c78aeeb3721c8673d651ae7519605460f6bc
Zonglei Dong <dzllikelsw@163.com>
2020-08-22 11:58:04 +0800

    Add demo code.

---
+++ netty-code-atguigu/src/main/java/com/atguigu/netty/protocoltcp/MessageProtocol.java
@@ -0,0 +1,25 @@
+package com.atguigu.netty.protocoltcp;
+
+//协议包
+public class MessageProtocol {
+    
+    private int len; //关键
+    
+    private byte[] content;
+
+    public int getLen() {
+        return len;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
